899172684fd8251e5a8ba1f59d2da8e69148c68a
андрей <p.aleksey2010@yandex.ru>
2023-03-03 13:29:41 +0300

    .\SaveTxt.java

---
+++ SaveTxt.java
@@ -0,0 +1,35 @@
+public class SaveTxt {
+
+    public void savePairs(Kinship k) {
+
+        try (FileWriter fw = new FileWriter("file.txt", false)) {
+
+            for (Node el : k.getPairs()) {
+                fw.write(el.toString());
+                fw.append('\n');
+            }
+            fw.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public void saveFamilyTree(ArrayList<ArrayList<Node>> family) {
+
+        try (FileWriter fw = new FileWriter("fileFamilyTree.txt", false)) {
+
+            for (ArrayList el : family) {
+                fw.write(el.toString());
+                fw.append('\n');
+                fw.append("************************************");
+                fw.append('\n');
+            }
+            fw.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+}
