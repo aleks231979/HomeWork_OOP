505e52018507de30390c4294d88f008504ed3c4e
андрей <p.aleksey2010@yandex.ru>
2023-03-03 13:17:42 +0300

    .\People.java

---
+++ People.java
@@ -0,0 +1,77 @@
+public class People extends Person implements Serializable {
+    private int id;
+    private String fname;
+    private String name;
+    private String getLname;
+    private LocalDate birghtday;
+    private String sex;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLname() {
+        return getLname;
+    }
+
+    public void setLname(String lname) {
+        this.getLname = lname;
+    }
+
+    public LocalDate getBirghtday() {
+        return birghtday;
+    }
+
+    public void setBirghtday(LocalDate birghtday) {
+        this.birghtday = birghtday;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "id=" + id +
+                ", fname='" + fname + '\'' +
+                ", name='" + name + '\'' +
+                ", lname='" + getLname + '\'' +
+                ", birghtday=" + birghtday +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
