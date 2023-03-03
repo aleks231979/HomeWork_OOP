bbff4cb22558a73ce57c356314618e9e9a2c4722
андрей <p.aleksey2010@yandex.ru>
2023-03-03 13:05:11 +0300

    .\Kinship.java

---
+++ Kinship.java
@@ -0,0 +1,37 @@
+public class Kinship implements Serializable {
+
+
+    private List<Node> pairs = new ArrayList<>();
+    private List<List<Node>> kinships = new ArrayList<>();
+
+
+    public Kinship(ArrayList<Node> pairs) {
+        this.pairs = pairs;
+
+    }
+
+    public Kinship() {
+        this(new ArrayList<>());
+
+    }
+
+    public void setKinship(People who, Relationship role, People whose) {
+        this.pairs.add(new Node(who, role, whose));
+        this.kinships.add(pairs);
+    }
+
+    public List<Node> getPairs() {
+        return pairs;
+    }
+
+    public List<List<Node>> getKinships() {
+        return kinships;
+    }
+
+    @Override
+    public String toString() {
+        return "Kinship{" +
+                "pairs=" + pairs +
+                '}';
+    }
+}
