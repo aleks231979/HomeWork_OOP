af33f6019e9933e7fd2dcf99604c16d28eb0392c
андрей <p.aleksey2010@yandex.ru>
2023-03-03 13:15:12 +0300

    .\Node.java

---
+++ Node.java
@@ -0,0 +1,17 @@
+public class Node implements Serializable {
+    People p1;
+    Relationship r;
+    People p2;
+
+    public Node(People p1, Relationship r, People p2) {
+        this.p1 = p1;
+        this.r = r;
+        this.p2 = p2;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s %s %s \n", p1.getFname(), p1.getFname(), r, p2.getFname(), p2.getName());
+    }
+}
